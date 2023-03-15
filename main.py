5d6b23c956eb52d98c173459459487406dc17c78
elliotsackman <elliotsackman@gmail.com>
2023-03-15 16:24:49 -0400

    Implemented Camera start, stop, and capture methods.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import time
 from filesharer import FileSharer
 
 Builder.load_file('frontend.kv')
@@ -9,13 +10,17 @@ Builder.load_file('frontend.kv')
 class CameraScreen(Screen):
 
     def start(self):
-        pass
+        self.ids.camera.play = True
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
-        pass
+        self.ids.camera.play = False
+        self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        current_time = time.strftime('%Y%m%d-%H%M')
+        filepath = 'files/' + current_time + '.png'
+        self.ids.camera.export_to_png(filepath)
 
 
 class ImageScreen(Screen):
