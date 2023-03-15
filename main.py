4512eda9fa93264890d8fab1ebc2da4bd38e4fdc
elliotsackman <elliotsackman@gmail.com>
2023-03-15 16:06:52 -0400

    Added filesharer class in separate file

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from filesharer import FileSharer
 
 Builder.load_file('frontend.kv')
 
 
 class CameraScreen(Screen):
-    pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def capture(self):
+        pass
 
 
 class ImageScreen(Screen):
